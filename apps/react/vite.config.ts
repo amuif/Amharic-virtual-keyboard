95f37ed99abb0d71166ea838373b5988ce9d3d2e
amuif <amudihayat@gmail.com>
2026-02-04 17:30:51 +0300

    feat:react now works waiting for props

--- apps/react/vite.config.ts
+++ apps/react/vite.config.ts
@@ -3,26 +3,36 @@ import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react(), dts()],
-  server:{
-    port:3000
+  plugins: [react(),
+  dts({
+    rollupTypes: true,
+    include: ['src/**/*'],
+    exclude: ['**/*.test.ts', '**/*.test.tsx']
+  })],
+  server: {
+    port: 3000
   },
   build: {
     lib: {
       entry: './src/index.tsx',
       name: 'AmharicKeyboardReact',
       fileName: (format) => `index.${format}.js`,
+      formats: ['es', 'umd']
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'jsxRuntime'
         },
       },
     },
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true,
+    minify: 'esbuild'
   }
 });
 
